c731d8a59edb8b884ae428f118c725c4a6d7a4ed
Pietro Franca <pietrofranca27@gmail.com>
2026-01-16 09:00:06 -0300

    feat: add authentication support to app and config

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,36 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask_cors import CORS
+import jwt
 from config import Config
 from database import db
+from routes.auth import auth_bp
 from routes.movies import movies_bp
 from routes.ratings import ratings_bp
 from dotenv import load_dotenv
 
 load_dotenv()
 
+def decode_token(token: str):
+  return jwt.decode(
+    token,
+    current_app.config["SECRET_KEY"],
+    algorithms=["HS256"],
+    options={"verify_exp": True}
+  )
+
 def create_app():
   app = Flask(__name__)
   app.config.from_object(Config)
 
-  CORS(app)
+  CORS(app,
+      supports_credentials=True,
+      origins=["http://localhost:5173"],
+      allow_headers=["Content-Type", "Authorization"],
+      expose_headers=["Set-Cookie"])
+  
   db.init_app(app)
 
+  app.register_blueprint(auth_bp) 
   app.register_blueprint(movies_bp)
   app.register_blueprint(ratings_bp)
 
--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 class Config:
+  SECRET_KEY = os.getenv("SECRET_KEY")
   SQLALCHEMY_DATABASE_URI = "sqlite:///movies.db"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   TMDB_API_KEY = os.getenv("TMDB_API_KEY")
